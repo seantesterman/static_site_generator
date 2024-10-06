46046de19443d9e009a6127a435c71b558416016
seantesterman <seantesterman@gmail.com>
2024-10-06 00:54:18 -0600

    7.0 Extract Links

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -5,6 +5,7 @@ from textnode import (
     text_type_italic,
     text_type_code,
 )
+import re
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
@@ -25,3 +26,11 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                 split_nodes.append(TextNode(sections[i], text_type))
         new_nodes.extend(split_nodes)
     return new_nodes
+
+def extract_markdown_images(text):
+    matches = re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    return matches
+
+def extract_markdown_links(text):
+    matches = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    return matches
