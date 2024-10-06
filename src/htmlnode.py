30a61fa7b8cdaf6d9fa3624428e554e9b92b686a
seantesterman <seantesterman@gmail.com>
2024-10-05 19:52:28 -0600

    4.0 ParentNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -40,4 +40,26 @@ class LeafNode(HTMLNode):
     def __repr__(self):
         return f"LeafNode({self.tag}, {self.value}, {self.props})"
         
+
+
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("Missing Tag")
+        if self.children is None:
+            raise ValueError("Parent Node Requires Child")
+        html_string = ""
+        for child in self.children:
+            html_string += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{html_string}</{self.tag}>"
+    
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
+            
         
+       
