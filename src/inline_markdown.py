5cea3f41d2033d79f28b5df2de40a88b62aa7696
seantesterman <seantesterman@gmail.com>
2024-10-07 13:04:32 -0600

    9.0 Text to TextNode

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -10,6 +10,15 @@ from textnode import (
 
 import re
 
+def text_to_textnodes(text):
+    nodes = [TextNode(text, text_type_text)]
+    nodes = split_nodes_delimiter(nodes, "**", text_type_bold)
+    nodes = split_nodes_delimiter(nodes, "*", text_type_italic)
+    nodes = split_nodes_delimiter(nodes, "`", text_type_code)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    return nodes
+
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
