35f22fc9ea964b4a42f1e7ae8987d50c5142d0a3
seantesterman <seantesterman@gmail.com>
2024-10-07 22:03:46 -0600

    12.0 Generate Page

--- src/main.py
+++ src/main.py
@@ -1,8 +1,29 @@
-from textnode import *
+import shutil
+import os
+
+from copystatic import copy_files_recursive
+from gencontent import generate_page
+
+dir_path_static = "./static"
+dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
 
 def main():
-   node = TextNode("abc", ".doc", "www.abc.com")
-   print(node)
+   print("Deleting public directory...")   
+   if os.path.exists(dir_path_public):
+       shutil.rmtree(dir_path_public)
+
+   print("Copying static files to public directory...")
+   copy_files_recursive(dir_path_static, dir_path_public)
+
+   print("Generating page...")
+   generate_page(
+       os.path.join(dir_path_content, "index.md"),
+       template_path,
+       os.path.join(dir_path_public, "index.html"),
+   )
+
 
 if __name__ == "__main__":
     main()
